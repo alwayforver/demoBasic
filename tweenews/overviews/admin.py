ddcf71b5304ecff9a29010b8bdb3d082eb6f1c3d
davidlimin <Min@MindeMacBook-Pro.local>
2014-10-18 12:58:26 -0700

    Website Start
    Start the website, create model class for tweet and news

---
+++ tweenews/overviews/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from overviews.models import News, Tweet
+admin.site.register(News)
+admin.site.register(Tweet)
+
+
+
+# Register your models here.
---
+++ tweenews/overviews/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    ID = models.BigIntegerField(primary_key = True)
+    user = models.CharField(max_length=30, verbose_name='tweet user')
+    raw_text = models.CharField(max_length=2000)
+    created_at = models.DateTimeField()
+    related_news = models.CharField(max_length=100, default = "", blank = True,)
+    is_retweet= models.BooleanField(default = False)
+    key_word = models.CharField(max_length=30,blank = True)
+    retweet_count = models.BigIntegerField(default=0)
+    hash_tags = models.CharField(max_length=30,blank = True, default = None)
+
+
+class News(models.Model):
+    ID = models.BigIntegerField(primary_key = True)
+    SOURCE_CHOICES = (("GF", "Google News Feed"),)
+    raw_text = models.CharField(max_length=2000)
+    key_word = models.CharField(max_length=30, blank=True)
+    source = models.CharField(max_length=2, choices = SOURCE_CHOICES, default = "GF")
+    created_at = models.DateTimeField()
+    related_Tweets = models.CharField(max_length=100, default = "")
+
+
+
+
+
+
+
+
+
