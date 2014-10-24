f66da401fe3c6c089b5ad3e1cee0a72c6f42d06d
Ridinstar <hcai6@illinois.edu>
2014-10-23 21:56:54 -0500

    model ID created_at workaround

--- tweenews/overviews/models.py
+++ tweenews/overviews/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 class News(models.Model):
-    ID = models.BigIntegerField(primary_key = True)
+    #ID = models.BigIntegerField(primary_key = True)
+    ID = models.CharField(max_length=200,primary_key = True)
     SOURCE_CHOICES = (("GF", "Google News Feed"),)
     raw_text = models.CharField(max_length=2000)
     key_word = models.CharField(max_length=30, blank=True)
     source = models.CharField(max_length=2, choices = SOURCE_CHOICES, default = "GF")
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default = datetime.now())
     title = models.CharField(max_length=100,default="testtitle")
     main_article = models.BooleanField(default=True)
     related_article = models.CharField(max_length=200,default="")
