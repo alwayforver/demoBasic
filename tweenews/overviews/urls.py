1903d00839f116615cd6fc5c2c9b296696968923
saberkun <solitarylord@gmail.com>
2014-11-03 01:00:15 -0600

    nothing big

--- tweenews/overviews/urls.py
+++ tweenews/overviews/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-
+from django.conf import settings
 from overviews import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     # url(r'^admin)
-    url(r'^news/$', views.news),
+    url(r'^news/$', views.news, name='news_home'),
     url(r'^tweet/(?P<news_ID>\d+)/$',views.tweet_with_news, name ='tweet_with_news'),
 )
--- tweenews/tweenews/urls.py
+++ tweenews/tweenews/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^overviews/', include('overviews.urls')),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^overviews/', include('overviews.urls', namespace="overview")),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, }),
+    )
