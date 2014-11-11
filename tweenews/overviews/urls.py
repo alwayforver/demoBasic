ad913aa16fa89d7aa493f785fc3dd98121aa281b
saberkun <solitarylord@gmail.com>
2014-11-11 00:43:00 -0600

    update news page: please debug news page on mysql

--- tweenews/overviews/urls.py
+++ tweenews/overviews/urls.py
@@ -7,7 +7,8 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     # url(r'^admin)
     url(r'^news/$', views.news, name='news_home'),
-    url(r'^tweet/id=(?P<tweet_id>\d+)/$', views.tweet_page, name='seller_bid_manager'),
+    url(r'^news/page(?P<pos>\d+)rank:(?P<rank_method>\d+)$', views.news, name='news_next'),
+    url(r'^tweet/id=(?P<tweet_id>\d+)/$', views.tweet_page, name='tweet_base'),
     url(r'^tweet/news=(?P<news_ID>\d+)/$',views.tweet_with_news, name ='tweet_with_news'),
 )
 urlpatterns += staticfiles_urlpatterns()
