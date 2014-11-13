cb4248a1935ac2b7f3eff89709a2debd6b10aec3
saberkun <solitarylord@gmail.com>
2014-11-13 11:56:24 -0600

    update tweet

--- tweenews/overviews/urls.py
+++ tweenews/overviews/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^news/page(?P<pos>\d+)rank:(?P<rank_method>\d+)$', views.news, name='news_next'),
     url(r'^tweet/id=(?P<tweet_id>\d+)/$', views.tweet_page, name='tweet_base'),
     url(r'^tweet/news=(?P<news_ID>\d+)/$',views.tweet_with_news, name ='tweet_with_news'),
+    url(r'^tweet/news=(?P<news_ID>\d+)page(?P<pos>\d+):(?P<counts>\d+)$', views.tweet_with_news, name='tweet_next'),
 )
 urlpatterns += staticfiles_urlpatterns()
