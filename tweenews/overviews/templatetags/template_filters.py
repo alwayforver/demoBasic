3dc9f16cafe847d37cf6c419468ac35155c91bce
saberkun <solitarylord@gmail.com>
2014-11-23 23:07:39 -0600

    ::::done

--- tweenews/overviews/templatetags/template_filters.py
+++ tweenews/overviews/templatetags/template_filters.py
@@ -14,8 +14,12 @@ def get_range(num):
 
 def plus(num1,num2):
 	return num1+num2
+
+def replace_mark(content):
+	return content.replace('::::', '\n')
 		
 register.filter('get_dict_item',get_dict_item)
 register.filter('get_dict_keys',get_dict_keys)
 register.filter('get_range',get_range)
 register.filter('plus',plus)
+register.filter('replace_mark', replace_mark)
