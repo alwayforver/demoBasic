cc73ebc53dbb04a983eafe203a98dde7f7052051
wztong <wztong1992@gmail.com>
2015-01-28 18:38:07 -0600

    models

--- tweenews/overviews/models.py
+++ tweenews/overviews/models.py
@@ -3,48 +3,54 @@ from datetime import datetime
 # Create your models here.
 
 class News(models.Model):
+
     ID = models.BigIntegerField(primary_key = True)
-    #ID = models.CharField(max_length=200,primary_key = True)
-    #SOURCE_CHOICES = (("GF", "Google News Feed"),)
-    raw_text = models.CharField(max_length=10000)
-    key_word = models.CharField(max_length=200, blank=True)
-    source = models.CharField(max_length=30,default="GF")
-   # source = models.CharField(max_length=2, choices = SOURCE_CHOICES, default = "GF")
-    #created_at = models.DateTimeField(default = datetime(2014, 1, 1, 1, 1, 1))
-    created_at = models.DateTimeField()
-    local_time_zone = models.CharField(max_length = 20)
-    title = models.CharField(max_length=200,default="testtitle")
-    main_article = models.BooleanField(default=True)
-    related_article = models.CharField(max_length=200,default="")
-    url = models.CharField(max_length=200)
+    url = models.CharField(max_length = 300, null = True, blank = True)
+    title = models.CharField(max_length = 200, null = True, blank = True)
+    source = models.CharField(max_length = 50, null = True, blank = True)
+    created_at = models.DateTimeField(null = True)
+    authors = models.CharField(max_length = 300, null = True, blank = True)
+    key_word = models.CharField(max_length = 200, null = True, blank = True)
+    snippet = models.CharField(max_length = 500, null = True, blank = True)
+    raw_text = models.CharField(max_length = 20000, null = True, blank = True)
 
     def __str__(self):
         return "News "+str(self.ID)
 
 
 class Tweet(models.Model):
+
     ID = models.BigIntegerField(primary_key = True)
-    #user = models.CharField(max_length=30, verbose_name='tweet user')
-    user = models.BigIntegerField(verbose_name='tweet user')
-    raw_text = models.CharField(max_length=200)
-    created_at = models.DateTimeField()
-    local_time_zone = models.CharField(max_length = 20)
-    is_retweet= models.BooleanField(default = False)
-#    key_word = models.CharField(max_length=30,blank = True)
-    retweet_count = models.BigIntegerField(default=0)
-    hash_tags = models.CharField(max_length=100, blank = True, default = "")
+    raw_text = models.CharField(max_length = 200, null = True, blank = True)
+    created_at = models.DateTimeField(null = True)
+    tweet_urls = models.CharField(max_length = 100, null = True, blank = True)
+    hash_tags = models.CharField(max_length = 50, null = True, blank = True)
+    retweet_id = models.BigIntegerField(null = True)
+    retweet_is_favorited = models.NullBooleanField(default = False)
+    retweet_favorite_count = models.IntegerField(null = True)
+    retweet_is_retweeted = models.NullBooleanField(default = False)
+    retweet_retweet_count = models.IntegerField(null = True)
+    is_favorited = models.NullBooleanField(default = False)
+    favorite_count = models.IntegerField(null = True)
+    is_retweet = models.NullBooleanField(default = False)
+    retweet_count = models.IntegerField(null = True)
+    user = models.BigIntegerField(null = True)
+    verified = models.NullBooleanField(default = False)
+    followers_count = models.IntegerField(null = True)
+    status_count = models.IntegerField(null = True)
+    friends_count = models.IntegerField(null = True)
+    user_favorites_count = models.IntegerField(null = True)
+    user_created_at = models.DateTimeField(null = True)
+    location = models.CharField(max_length = 100, null = True, blank = True)
+    coordinates = models.CharField(max_length = 40, null = True, blank = True)
+    source = models.CharField(max_length = 30, null = True, blank = True)
+    mentions = models.CharField(max_length = 100, null = True, blank = True)
+    reply_user_id = models.BigIntegerField(null = True)
+    reply_status_id = models.BigIntegerField(null = True)
+
     related_news = models.ManyToManyField(News)
 
     def __str__(self):
-	return "Tweet "+str(self.ID)+ ' '  + str(self.raw_text.encode("utf-8","ignore"))
-
-
-
-
-
-
-
-
-
+    return "Tweet "+str(self.ID)+ ' '  + str(self.raw_text.encode("utf-8","ignore"))
 
 
