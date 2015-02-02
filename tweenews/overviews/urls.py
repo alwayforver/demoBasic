b53453b78143eb352d86f3cbfdc5ca181f18f90e
saberkun <solitarylord@gmail.com>
2015-02-01 19:06:23 -0600

    test

--- tweenews/overviews/urls.py
+++ tweenews/overviews/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # url(r'^admin)
     url(r'^news/$', views.news, name='news_home'),
     url(r'^news/page(?P<pos>\d+)rank:(?P<rank_method>\d+)$', views.news, name='news_next'),
+    url(r'^news/page(?P<pos>\d+)rank:(?P<rank_method>\d+)&q=(?P<q>\w*)$', views.news, name='news_next'),
     url(r'^tweet/id=(?P<tweet_id>\d+)/$', views.tweet_page, name='tweet_base'),
     url(r'^tweet/news=(?P<news_ID>\d+)/$',views.tweet_with_news, name ='tweet_with_news'),
     url(r'^tweet/news=(?P<news_ID>\d+)page(?P<pos>\d+):(?P<counts>\d+)$', views.tweet_with_news, name='tweet_next'),
