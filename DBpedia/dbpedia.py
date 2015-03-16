b82905caf644d132c77837133b7a0d54c9555f18
alwayforver <wangjj.smile@gmail.com>
2015-03-16 17:07:11 -0500

    delete pyc

--- DBpedia/dbpedia.py
+++ DBpedia/dbpedia.py
@@ -5,42 +5,46 @@ import json
 
 def EntityExtraction(text):
 
-	url = 'http://localhost:2222/rest/annotate'    #local service
-	params = {'confidence': 0.3, 'support': 10, 'text': text}
-	reqheaders = {'accept': 'application/json'}
+    url = 'http://localhost:2222/rest/annotate'    #local service
+    params = {'confidence': 0.5, 'support': 10, 'text': text}
+    reqheaders = {'accept': 'application/json'}
 
-	response = requests.post(url, data=params, headers=reqheaders)
+    response = requests.post(url, data=params, headers=reqheaders)
 
-	results = response.json()
+    results = response.json()
 
-	entities = {}
+    entities = {}
 
-	if 'Resources' in results:
-		for result in results['Resources']:
-			entity = result['@surfaceForm']
-			types = result['@types']
+    if 'Resources' in results:
+        for result in results['Resources']:
+            entity = result['@surfaceForm']
+            types = result['@types']
 
-			if 'Person' in types:
-				entity += ':person'
-			elif 'Place' in types:
-				entity += ':place'
-			elif 'Organisation' in types:
-				entity += ':org'
+            if 'DBpedia:Person' in types:
+                entity += ':person'
+            elif 'DBpedia:Place' in types:
+                entity += ':place'
+            elif 'DBpedia:Organisation' in types:
+                entity += ':org'
 
-			if entity in entities:
-				entities[entity] += 1
-			else:
-				entities[entity] = 1
+            if entity in entities:
+                entities[entity] += 1
+            else:
+                entities[entity] = 1
 
-	entities_string = ''
-	for (entity, count) in entities.items():
-		entities_string += entity + ':' + str(count) + '\t'
-		
-	return entities_string
+    entities_string = ''
+    for (entity, count) in entities.items():
+        entities_string += entity + ':' + str(count) + '\t'
+        
+    return entities_string
 
 
 if __name__ =='__main__':
 
-	text = 'President Obama called Wednesday on Congress to extend a tax break for students included in last year\'s economic stimulus package, arguing that the policy provides more generous assistance.'
-	print EntityExtraction(text)
+
+    for i in range(10):
+        text = 'President Obama called Wednesday on Congress to extend a tax break for students included in last year\'s economic stimulus package, arguing that the policy provides more generous assistance.'
+        print EntityExtraction(text)
+        if i%1000==1:
+            print i
 
